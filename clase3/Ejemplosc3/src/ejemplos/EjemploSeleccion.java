600a9271da6633bf0bb6ddfd21a87937c32d7725
Jason Castillo <jasson_72@hotmail.com>
2018-04-19 11:38:53 -0500

    agregando ejercicios semana 3

---
+++ clase3/Ejemplosc3/src/ejemplos/EjemploSeleccion.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ /*
+Crear un programa que permitain gresar una calificación por teclado,
+Si la calificación es mayor o igual a 5 y la calificación es menor a 10
+Se debe presentar un mensaje que diga usted está en supletorios.
+De lo contrario se presenta un mensaje con el valor de la calificación 
+dividido para dos.
+ */
+package ejemplos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EjemploSeleccion {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double calificacion;
+        System.out.println("Ingrese su calificación");
+        calificacion = entrada.nextDouble();
+
+        if (calificacion >= 5 && calificacion < 10) {
+            System.out.printf("Usted está en supletorios");
+        }else{
+            
+            //System.out.printf("%d",calificacion/2);
+            double resultado = calificacion/2;
+            System.out.printf("%.2f",resultado);
+        }
+    }
+
+}
---
+++ clase3/Ejemplosc3/src/ejemplos/SeleccionAnidada.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class SeleccionAnidada {
+
+    public static void main(String[] args) {
+        // documentacion
+        Scanner entrada = new Scanner(System.in);
+        int calificacion;
+        String estudiante;
+        System.out.println("Ingrese sus nombres:");
+        estudiante = entrada.nextLine();
+        System.out.println("Ingrese su calificación");
+        calificacion = entrada.nextInt();
+        if (calificacion >= 90) {
+            System.out.printf("Estudiante:%s\nexcelente\ncon calificación\t\n%d\n",estudiante, calificacion);
+        } else {
+            if (calificacion < 90 && calificacion >= 80) {
+                System.out.printf("Estudiante:%s\nMuy buena\ncon calificación\t\n%d\n", estudiante,calificacion);
+            } else {
+                if (calificacion < 80 && calificacion >= 50) {
+                    System.out.printf("Estudiante:%s\nRegular\ncon calificación\t\n%d\n", estudiante,calificacion);
+
+                } else {
+                    System.out.printf("Estudiante:%s\nReprobado\ncon calificación\t\n%d\n", estudiante, calificacion);
+                }
+            }
+        }
+        
+    }
+}
